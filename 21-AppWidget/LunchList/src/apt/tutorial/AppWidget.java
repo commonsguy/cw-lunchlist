e5274382a161a4d8d6e35b74e731fa9f2669f587
Mark Murphy <mmurphy@commonsware.com>
2011-04-02 15:57:48 -0400

    New tutorial

---
+++ 21-AppWidget/LunchList/src/apt/tutorial/AppWidget.java
@@ -0,0 +1,15 @@
+package apt.tutorial;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class AppWidget extends AppWidgetProvider {
+	@Override
+	public void onUpdate(Context ctxt,
+												AppWidgetManager mgr,
+												int[] appWidgetIds) {
+		ctxt.startService(new Intent(ctxt, WidgetService.class));
+	}
+}
